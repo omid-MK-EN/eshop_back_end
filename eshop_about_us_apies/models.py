637d35a5a2f36cfd63c72c9724da1795e1723354
omid <admin@gmail.com>
2022-04-05 13:47:39 +0300

    created fields of eshop_about_us_apies model

--- eshop_about_us_apies/models.py
+++ eshop_about_us_apies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class AboutUs(models.Model):
+    company_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    tel = models.CharField(max_length=30)
+    fax = models.CharField(max_length=30)
+    mobile =models.CharField(max_length=30)
+    compane_descriptions = models.TextField()
+    adress = models.TextField()
+    logo = models.ImageField(upload_to = "eshop_about_us_log/")
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self) :
+        return self.company_name
