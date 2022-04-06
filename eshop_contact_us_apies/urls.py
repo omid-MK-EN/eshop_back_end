2f9e0127af4f6870491c5be2356d9ec4e02eed21
omid <admin@gmail.com>
2022-04-06 10:48:47 +0300

    continue project to complete contact-us model

---
+++ eshop_contact_us_apies/urls.py
@@ -0,0 +1,11 @@
+# from rest_framework import routers
+# from .views import EshopAboutUsViewSet
+# from django.urls import path,include
+#
+# router = routers.SimpleRouter()
+# router.register('about_us', EshopAboutUsViewSet,basename="about_us")
+#
+#
+# urlpatterns = [
+#    path("",include(router.urls)),
+# ]
