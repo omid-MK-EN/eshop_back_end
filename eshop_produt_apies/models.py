8312ccf7e967962a312f438aca92a5497f66ee05
omid <admin@gmail.com>
2022-04-08 07:54:29 +0300

    add product properties to ProdutdRelatedPhotos

--- eshop_produt_apies/models.py
+++ eshop_produt_apies/models.py
@@ -49,6 +49,7 @@ class ProductRelatedPhotos(models.Model):
     name = models.CharField(max_length=20)
     image = models.ImageField(upload_to="related_images/", null=True, blank=True)
     active = models.BooleanField(default=True)
+    product= models.ForeignKey("EshopProduct",on_delete=models.CASCADE,related_name="product_related",null=True,blank=True)
 
     def __str__(self):
         return self.name
